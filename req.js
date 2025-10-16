5210b81b8f22fc5196a4e318e35b86a8112b4d01
harounaKane <harouna.Kane@hotmail.fr>
2025-10-16 13:53:22 +0200

    ficher donnees etudiants

---
+++ req.js
@@ -0,0 +1,13 @@
+const t = [
+  {
+    prenom: "Titi",
+    formation: "DWWM",
+    age: 25,
+    notes: [13, 15, 17],
+  },
+  {
+    prenom: "Tata",
+    formation: "CDA",
+    centre: "M2I"
+  }
+];
