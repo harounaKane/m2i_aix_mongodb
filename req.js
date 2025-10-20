f511d0f80a9596a5fec3d3e70e8e77d0f70d358b
harounaKane <harouna.Kane@hotmail.fr>
2025-10-20 12:11:10 +0200

    ficher TP_2 q 5

--- req.js
+++ req.js
@@ -67,4 +67,99 @@ db.commandes.aggregate([
     ville: "$client.adresse.ville",
     CA: 1
   }}
-])
+]);
+
+
+// Panier moyen global
+db.commandes.aggregate([
+  {$match: {statut: {$in: ["livrée", "expédiée", "payée"]}}},
+  {$group: {_id: null, caTotal:{$sum: "$totalTTC"}, nbCmd: {$sum: 1}}},
+  {$project: {_id: 0, panierMoyen: {$round: [{$divide: ["$caTotal", "$nbCmd"]}, 2]}}}
+]);
+
+
+// Taux d’annulation
+db.commandes.aggregate([
+  {$group: {
+    _id: null, 
+    nbCmd: {$sum: 1},
+    totalAnnul: {
+      $sum: {
+        $cond: [
+          {$eq: ["$statut", "annulée"]}, 1, 0
+        ]
+      }
+    }
+  }},
+  {$project: {
+    _id: 0, 
+    totalAnnul: 1,
+    nbCmd: 1,
+    tauxAnnule: {
+      $round: [
+        {
+          $multiply: [
+            {$divide: ["$totalAnnul", "$nbCmd"]}, 100
+          ]
+        }, 2
+      ]
+    }
+  }}
+]);
+
+
+// Réachat sur 90 jours
+db.commandes.aggregate([
+  {$match: {createdAt: {$gte: new Date(Date.now() - 90*24*60*60*1000)}}},
+  {$group: {_id: "$clientId", nbCmd: {$sum: 1}}},
+  {$match: {nbCmd: {$gte: 2}}},
+  {$lookup: {from: "clients", localField: "_id", foreignField: "_id", as: "client"}},
+  {$unwind: "$client"},
+  {$project: {
+    _id: 0,
+    prenom: "$client.prenom",
+    nom: "$client.nom",
+    email: "$client.email",
+    nbCmd: 1
+  }}
+]);
+
+
+// Ville la plus rentable, puis top 3
+db.commandes.aggregate([
+  {$match: {statut: {$ne: "annulée"}}},
+  {$lookup: {from: "clients", localField: "clientId", foreignField: "_id", as: "client"}},
+  {$unwind: "$client"},
+  {$group: {_id: "$client.adresse.ville", caTotal: {$sum: "$totalTTC"}}},
+  {$sort: {caTotal: -1}},
+  {$limit: 3},
+  {$project: {
+    _id: 0,
+    ville: "$_id",
+    caTotal: {$round:["$caTotal", 2]}
+  }}
+]);
+
+// Statistiques par catégorie produit - CA et quantités vendues par categorie
+db.commandes.aggregate([
+  {$match: {statut: {$ne: "annulée"}}},
+  {$unwind: "$items"},
+  {$lookup: {from: "produits", localField: "items.produitId", foreignField: "_id", as: "prod"}},
+  {$unwind: "$prod"},
+  {$group: {
+    _id: "$prod.categorie", 
+    qtTotal: {$sum: "$items.qte" }, 
+    caTotal: {$sum: {$multiply: ["$items.qte", "$items.prixUnitaire"]}}
+  }},
+  {$project: {_id: 0, categorie: "$_id", qtTotal: 1, caTotal: 1}}
+]);
+
+
+// Clients sans commande
+db.clients.aggregate([
+  {$lookup: {from: "commandes", localField: "_id", foreignField: "clientId", as: "cmd"}},
+  {$match: {cmd: {$size: 0}}},
+  {$project: {
+    nom: 1, prenom: 1
+  }}
+]);
